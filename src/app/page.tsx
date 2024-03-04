cf0793c7937313b07fc6d20ecb64746ba2cbbf14
osamuel-cardoso <samuelcardoso555@gmail.com>
2024-03-04 17:44:19 -0300

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,3 @@
-import { Footer } from './components/Footer'
-import { Carousel } from './components/View/components/Carousel'
-import { Classes } from './components/View/components/Classes'
-import { Delivery } from './components/View/components/Delivery'
-import { Feedbacks } from './components/View/components/Feedbacks'
-import { Header } from './components/View/components/Header'
-import { Hero } from './components/View/components/Hero'
-import { Inside } from './components/View/components/Inside'
-import { Pricing } from './components/View/components/Pricing'
-import { Questions } from './components/View/components/Questions'
-
 export default function Initial() {
-    return (
-        <>
-            <noscript>
-                <iframe
-                    src="https://www.googletagmanager.com/ns.html?id=GTM-MFXQDVC"
-                    height="0"
-                    width="0"
-                    style={{
-                        display: 'none',
-                        visibility: 'hidden',
-                    }}
-                ></iframe>
-            </noscript>
-            <Header />
-            <Hero />
-            <Inside />
-            <Classes />
-            <Carousel />
-            <Delivery />
-            <Pricing />
-            <Feedbacks />
-            <Questions />
-            <Footer />
-        </>
-    )
+    return <span>...</span>
 }
