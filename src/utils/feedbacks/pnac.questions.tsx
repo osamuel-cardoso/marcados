d8f8d01176b38a8c9b75597cf10c289f49e7ea68
osamuel-cardoso <108777753+osamuel-cardoso@users.noreply.github.com>
2024-04-02 23:22:12 -0300

    Update pnac.questions.tsx

--- src/utils/feedbacks/pnac.questions.tsx
+++ src/utils/feedbacks/pnac.questions.tsx
@@ -1,27 +1,22 @@
 export const pnacQuestions = [
     {
         title: 'QUANTO TEMPO DE ACESSO TEREI?',
-        content:
-            'Vamos explorar o conceito de posicionamento de forma clara e acessível, garantindo que você tenha uma compreensão sólida do que é e por que é importante para o sucesso do seu negócio criativo.',
+        content: '',
     },
     {
         title: 'COMO RECEBO OS DADOS DE ACESSO?',
-        content:
-            'Você receberá diretrizes detalhadas sobre como criar um plano estratégico de atração de clientes que envolva seu público de maneira significativa e eficaz, sem depender de gastos com anúncios pagos.',
+        content: '',
     },
     {
         title: 'PARA QUEM É O PNAC?',
-        content:
-            'Não só explicaremos como exercer um posicionamento estratégico, mas também daremos orientação prática e exercícios para que você possa aplicar imediatamente o que aprendeu.',
+        content: '',
     },
     {
         title: 'O PNAC SERVE PARA INICIANTES?',
-        content:
-            'Vamos explorar o conceito de posicionamento de forma clara e acessível, garantindo que você tenha uma compreensão sólida do que é e por que é importante para o sucesso do seu negócio criativo.',
+        content: '',
     },
     {
         title: 'VOU FICAR PERDIDO(A) LÁ DENTRO?',
-        content:
-            'Este curso inclui técnicas de pesquisa de mercado e métodos práticos para entender as dores e desejos do seu nicho. Você estará preparado para criar soluções que atendam às necessidades específicas do seu público.',
+        content: '',
     },
 ]
